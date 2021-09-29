a0ab537081b887b1cf699c8513505d330c15ea33
devlulcas <lucasalvesrego.contato@gmail.com>
2021-09-28 21:57:12 -0300

    add: controller class that does basicaly everything

---
+++ src/Controller/SchoolTaskController.kt
@@ -0,0 +1,25 @@
+package Controller
+
+import Model.SchoolTask
+
+class SchoolTaskController(
+    var taskList: ArrayList<SchoolTask> = arrayListOf()
+) {
+
+    public fun addTask(title: String, creationDate: String, deliverDate: String, observation: String) {
+        val task: SchoolTask = SchoolTask(title, creationDate, deliverDate, observation)
+        taskList.add(task)
+    }
+
+    public fun updateTask(taskIndex: Int, newTitle: String, newDeliverDate: String, newObservation: String) {
+        val oldTask: SchoolTask = taskList.elementAt(taskIndex)
+        oldTask.title = newTitle
+        oldTask.deliverDate = newDeliverDate
+        oldTask.observation = newObservation
+    }
+
+    public fun deleteTask(taskIndex: Int) {
+        val dyingTask = taskList[taskIndex]
+        taskList.remove(dyingTask)
+    }
+}
